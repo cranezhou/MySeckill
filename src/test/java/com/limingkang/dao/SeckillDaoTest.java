88dcbad295bf6fe910e5aabcf955192cde0071d7
李明康 <limingkang@thinkit.cn>
2017-07-21 10:50:48 +0800

    添加测试目录

---
+++ src/test/java/com/limingkang/dao/SeckillDaoTest.java
@@ -0,0 +1,20 @@
+package com.limingkang.dao;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class SeckillDaoTest {
+    @Test
+    public void reduceNumber() throws Exception {
+    }
+
+    @Test
+    public void queryById() throws Exception {
+    }
+
+    @Test
+    public void queryAll() throws Exception {
+    }
+
+}
