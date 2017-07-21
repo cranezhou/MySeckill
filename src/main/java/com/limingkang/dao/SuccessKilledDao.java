68ad6193bbc351b2eaa318cf64ec38824ba763c8
李明康 <limingkang@thinkit.cn>
2017-07-21 17:42:06 +0800

    添加参数注解

--- src/main/java/com/limingkang/dao/SuccessKilledDao.java
+++ src/main/java/com/limingkang/dao/SuccessKilledDao.java
@@ -1,6 +1,7 @@
 package com.limingkang.dao;
 
 import com.limingkang.entity.SuccessKilled;
+import org.apache.ibatis.annotations.Param;
 
 /**
  * Created by thinkit on 2017/7/20.
@@ -12,12 +13,12 @@ public interface SuccessKilledDao {
      * @param userPhone
      * @return
      */
-    int insertSuccessKilled(long seckillId, long userPhone);
+    int insertSuccessKilled(@Param("seckillId") long seckillId,@Param("userPhone") long userPhone);
 
     /**
      * 根据id查询购买明细，并携带秒杀产品对象属性
      * @param seckillId
      * @return
      */
-    SuccessKilled queryByIdWithSeckill(long seckillId);
+    SuccessKilled queryByIdWithSeckill(@Param("seckillId") long seckillId,@Param("userPhone") long userPhone);
 }
